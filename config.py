4026692981a33964f69f5b3ece1be3cd0cc6d304
Fengzhe <1k.magnus@gmail.com>
2015-12-10 21:07:04 +0800

    最终解决方案还是悲惨的只用来输出一个txt文件 然后手动输入到迅雷
    迅雷我恨你

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ def load_config(filename):
 
 def save_config(json_dict, filename):
     with open(filename, 'w', encoding='utf-8') as file:
-        text = json.dumps(json_dict)
+        text = json.dumps(json_dict, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
         file.write(text)
 
 if __name__ == "__main__":
@@ -20,9 +20,136 @@ if __name__ == "__main__":
     js_dict = dict()
     js_dict['name'] = "冰与火之歌"
     js_dict['url'] = "http://www.zimuzu.tv/resource/list/10733"
-    js_dict['season'] = 3
-    js_dict['episode'] = 1
+    js_dict['season'] = 5
+    js_dict['episode'] = 10
     file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "生活大爆炸"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/11005"
+    js_dict['season'] = 9
+    js_dict['episode'] = 9
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "摩登家庭"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/11010"
+    js_dict['season'] = 7
+    js_dict['episode'] = 6
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "Community"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/11018"
+    js_dict['season'] = 6
+    js_dict['episode'] = 13
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "破产姐妹"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/11103"
+    js_dict['season'] = 5
+    js_dict['episode'] = 3
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "Dr Ken"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/33666"
+    js_dict['season'] = 1
+    js_dict['episode'] = 7
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "Supergirl"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/33549"
+    js_dict['season'] = 1
+    js_dict['episode'] = 5
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "疑犯追踪"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/11009"
+    js_dict['season'] = 5
+    js_dict['episode'] = 0
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "Supernatural"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/11015"
+    js_dict['season'] = 10
+    js_dict['episode'] = 0
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "Flash"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/32235"
+    js_dict['season'] = 2
+    js_dict['episode'] = 0
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "绿箭"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/32235"
+    js_dict['season'] = 3
+    js_dict['episode'] = 3
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "哥谭"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/32097"
+    js_dict['season'] = 1
+    js_dict['episode'] = 3
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "灵书妙探"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/10996"
+    js_dict['season'] = 7
+    js_dict['episode'] = 6
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "硅谷"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/31801"
+    js_dict['season'] = 2
+    js_dict['episode'] = 0
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "南方公园"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/10490"
+    js_dict['season'] = 18
+    js_dict['episode'] = 5
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "格林"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/11080"
+    js_dict['season'] = 4
+    js_dict['episode'] = 0
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "演绎法"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/26898"
+    js_dict['season'] = 3
+    js_dict['episode'] = 0
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "神盾局特工"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/30675"
+    js_dict['season'] = 3
+    js_dict['episode'] = 0
+    file_dict.append(js_dict)
+
+    js_dict = dict()
+    js_dict['name'] = "纸牌屋"
+    js_dict['url'] = "http://www.zimuzu.tv/resource/list/28793"
+    js_dict['season'] = 1
+    js_dict['episode'] = 0
+    file_dict.append(js_dict)
+
     save_config(file_dict, "zimuzu.conf")
     # js_dict = load_config("zimuzu.conf")
     # print(js_dict)
--- zimuzu_spider.py
+++ zimuzu_spider.py
@@ -5,17 +5,16 @@ from bs4 import BeautifulSoup
 import uuid
 import time
 import re
-# import xmlrpc.client
-import subprocess
-from config import load_config
-import os
+from config import load_config, save_config
+from collections import OrderedDict
+import datetime
 
 
 config_name = "zimuzu.conf"
 
 
 def sign(account, password):
-    print('signing...')
+    print('Signing...')
     client_id = str(uuid.uuid1())
     headers = {'Accept': ' application/json, text/javascript, */*; q=0.01',
                'X-DevTools-Emulate-Network-Conditions-Client-Id': client_id,
@@ -29,16 +28,13 @@ def sign(account, password):
                'Accept-Encoding': ' gzip, deflate, sdch',
                'Accept-Language': ' zh-CN,zh;q=0.8'}
     res_headers = requests.get('http://www.zimuzu.tv/user/login', headers=headers).headers
-    print(res_headers)
     session = res_headers['set-cookie'][10:36]
-    print(session)
     headers['Cookie'] = 'PHPSESSID='+session+'; CNZZDATA1254180690=784053439-1449042469-%7C1449047872'
     data = {'account': account,
             'password': password,
             'remember': '1',
             'url_back': 'http://www.zimuzu.tv/user/sign'}
     res = requests.post('http://www.zimuzu.tv/User/Login/ajaxLogin', data=data, headers=headers)
-    print(res.headers)
     cookie = res.headers['set-cookie']
     cookie = cookie.replace('GINFO=deleted;', '').replace('GKEY=deleted;', '')
     GINFO = re.search('GINFO=uid[^;]+', cookie).group(0)+";"
@@ -54,18 +50,23 @@ def sign(account, password):
     #                                                           "sign failed! " + str(content)), content['status']
 
     playlist = load_config(config_name)
-
+    alluri = ""
     for play in playlist:
-
+        seasons = dict()
         res = requests.get(play["url"], headers=headers)
-        print(res.content)
+        # print(res.content)
         soup = BeautifulSoup(res.content, 'html.parser')
-        alluri = ""
         for one in soup.find_all('li', format='HR-HDTV'):
-            if (int(one['season']) == play['season'] and int(one['episode']) > play['episode']) or \
-                    (int(one['season']) > play['season'] and int(one['episode']) != 0):
+            season = int(one['season'])
+            episode = int(one['episode'])
+            if (season == play['season'] and episode > play['episode']) or \
+                    (season > play['season'] and episode != 0):
                 link = one.find('a', href=re.compile('ed2k:'))
-                alluri += str(link['href']) + "\n"
+                uri = str(link['href'])
+                if season not in seasons:
+                    seasons[season] = dict()
+                seasons[season][episode] = uri
+                # alluri += str(link['href']) + "\n"
 
                 # I have trouble to download magnet with aria2, I will try something else.
                 # with xmlrpc.client.ServerProxy("http://localhost:6800/rpc") as proxy:
@@ -73,7 +74,42 @@ def sign(account, password):
                 #     print(val)
 
                 # Too many times access Xunlei will cause "Verification code required"
-                # os.system('python2 C:\\Users\\Administrator\\Documents\\xunlei-lixian\\lixian_cli.py download --tool=a2 "{}"'.format(str(link['href'])))
-                print(str(link['href']))
-        # subprocess.call(["C:\Program Files (x86)\Thunder Network\Thunder\Program\Thunder.exe", alluri],
-        #                 shell=True)
+
+                # os.system('python2 E:\\xunlei-lixian\\lixian_cli.py download '
+                #           '--tool=aria2 --continue "{}"'.format(str(link['href'])))
+        print("Got all episodes of {}".format(play['name']))
+        # os.system('C:\\Python27\\python.exe E:\\xunlei-lixian\\lixian_cli.py login')
+        seasons = OrderedDict(sorted(seasons.items()))
+        for season in seasons:
+            seasons[season] = OrderedDict(sorted(seasons[season].items()))
+        for season in seasons:
+            for episode in seasons[season]:
+                # uri = seasons[season][episode]
+                # exit_code = -1
+                # retry_times = 0
+                # while exit_code != 0 and retry_times < 10000:
+                #     print("Start downloading {} season {} episode {}".format(play['name'], season, episode))
+                #     exit_code = os.system('C:\\Python27\\python.exe E:\\xunlei-lixian\\lixian_cli.py '
+                #                           'download --tool=aria2 --continue "{}"'.format(uri))
+                #     # exit_code = subprocess.call(['C:\\Python27\\python.exe', 'E:\\xunlei-lixian\\lixian_cli.py',
+                #     #                              'download', '--tool=aria2', '--continue', '"{}"'.format(uri)])
+                #     print(exit_code)
+                #     if exit_code != 0:
+                #         retry_times += 1
+                #         print("Something wrong. Wait one minute...")
+                #         sleep(60)
+                #         print("Start retry {}".format(retry_times))
+                #
+                #     else:
+                #         print("{} season {} episode {} downloaded".format(play['name'], season, episode))
+                uri = seasons[season][episode]
+                alluri += uri + "\n\r"
+                play['season'] = season
+                play['episode'] = episode
+                save_config(playlist, config_name)
+
+    timenow = datetime.datetime.now().strftime("%Y-%m-%d")
+    filename = "zimuzu_txt\\zimuzu_{}.txt".format(timenow)
+    print(filename)
+    with open(filename, 'a', encoding='utf-8') as file:
+        file.write(alluri)
