f0fc45309e37f2fd1b01adbfb8bf0bcc8b81b666
Fengzhe <1k.magnus@gmail.com>
2015-12-11 13:50:27 +0800

    模块化程序

--- zimuzu_spider.py
+++ zimuzu_spider.py
@@ -13,49 +13,54 @@ import datetime
 config_name = "zimuzu.conf"
 
 
-def sign(account, password):
-    print('Signing...')
-    client_id = str(uuid.uuid1())
-    headers = {'Accept': ' application/json, text/javascript, */*; q=0.01',
-               'X-DevTools-Emulate-Network-Conditions-Client-Id': client_id,
-               'X-Requested-With': 'XMLHttpRequest',
-               'X-FirePHP-Version': '0.0.6',
-               'Host': 'www.zimuzu.tv',
-               'Origin': 'http://www.zimuzu.tv',
-               'Referer': 'http://www.zimuzu.tv/user/login',
-               'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/42.0.2311.90 Safari/537.36',
-               'Accept-Encoding': ' gzip, deflate, sdch',
-               'Accept-Language': ' zh-CN,zh;q=0.8'}
-    res_headers = requests.get('http://www.zimuzu.tv/user/login', headers=headers).headers
-    session = res_headers['set-cookie'][10:36]
-    headers['Cookie'] = 'PHPSESSID='+session+'; CNZZDATA1254180690=784053439-1449042469-%7C1449047872'
-    data = {'account': account,
-            'password': password,
-            'remember': '1',
-            'url_back': 'http://www.zimuzu.tv/user/sign'}
-    res = requests.post('http://www.zimuzu.tv/User/Login/ajaxLogin', data=data, headers=headers)
-    cookie = res.headers['set-cookie']
-    cookie = cookie.replace('GINFO=deleted;', '').replace('GKEY=deleted;', '')
-    GINFO = re.search('GINFO=uid[^;]+', cookie).group(0)+";"
-    GKEY = re.search('GKEY=[^;]+', cookie).group(0)+";"
-    CPS = 'yhd%2F'+str(int(time.time()))+";"
-    Cookie = ' PHPSESSID='+session+'; '+CPS+(GINFO+GKEY)*3
-    headers['Cookie'] = Cookie
-    # requests.get("http://www.zimuzu.tv/user/sign", headers=headers).content
-    # print('wait for 20 seconds...')
-    # time.sleep(20)
-    # content = requests.get("http://www.zimuzu.tv/user/sign/dosign", headers=headers).json()
-    # print("sign success! ") if content['data'] != False else ("signed! " if content['data'] == False else
-    #                                                           "sign failed! " + str(content)), content['status']
+class Zimuzu_site:
+    def __init__(self, account, password):
+        self.headers = dict()
+        sign(account, password)
 
+    def sign(account, password):
+        print('Signing...')
+        client_id = str(uuid.uuid1())
+        self.headers = {'Accept': ' application/json, text/javascript, */*; q=0.01',
+                   'X-DevTools-Emulate-Network-Conditions-Client-Id': client_id,
+                   'X-Requested-With': 'XMLHttpRequest',
+                   'X-FirePHP-Version': '0.0.6',
+                   'Host': 'www.zimuzu.tv',
+                   'Origin': 'http://www.zimuzu.tv',
+                   'Referer': 'http://www.zimuzu.tv/user/login',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                 'Chrome/42.0.2311.90 Safari/537.36',
+                   'Accept-Encoding': ' gzip, deflate, sdch',
+                   'Accept-Language': ' zh-CN,zh;q=0.8'}
+        res_headers = requests.get('http://www.zimuzu.tv/user/login', headers=headers).headers
+        session = res_headers['set-cookie'][10:36]
+        self.headers['Cookie'] = 'PHPSESSID='+session+'; CNZZDATA1254180690=784053439-1449042469-%7C1449047872'
+        data = {'account': account,
+                'password': password,
+                'remember': '1',
+                'url_back': 'http://www.zimuzu.tv/user/sign'}
+        res = requests.post('http://www.zimuzu.tv/User/Login/ajaxLogin', data=data, headers=headers)
+        cookie = res.headers['set-cookie']
+        cookie = cookie.replace('GINFO=deleted;', '').replace('GKEY=deleted;', '')
+        GINFO = re.search('GINFO=uid[^;]+', cookie).group(0)+";"
+        GKEY = re.search('GKEY=[^;]+', cookie).group(0)+";"
+        CPS = 'yhd%2F'+str(int(time.time()))+";"
+        Cookie = ' PHPSESSID='+session+'; '+CPS+(GINFO+GKEY)*3
+        self.headers['Cookie'] = Cookie
+
+    def get_url(url):
+        res = requests.get(play["url"], headers=headers)
+        return res.content
+
+def get_plays(account, password)
+    site = Zimuzu_site(account,password)
     playlist = load_config(config_name)
     alluri = ""
     for play in playlist:
         seasons = dict()
-        res = requests.get(play["url"], headers=headers)
+        webpage = site.get_url(play["url"])
         # print(res.content)
-        soup = BeautifulSoup(res.content, 'html.parser')
+        soup = BeautifulSoup(webpage, 'html.parser')
         for one in soup.find_all('li', format='HR-HDTV'):
             season = int(one['season'])
             episode = int(one['episode'])
