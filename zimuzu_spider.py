bfae6af12f47122f680a5f5663dfc8690e48c154
jinsheng <jinsheng@jibiao.org>
2015-12-11 17:03:50 +0800

    一些格式错误的修正 动漫花园爬虫函数化

--- zimuzu_spider.py
+++ zimuzu_spider.py
@@ -3,7 +3,6 @@
 import requests
 from bs4 import BeautifulSoup
 import uuid
-import time
 import re
 from config import load_config, save_config
 from collections import OrderedDict
@@ -12,7 +11,7 @@ import os
 import time
 
 
-class Zimuzu_site:
+class ZimuzuSite:
     def __init__(self, filename=""):
         self.headers = dict()
         if filename != "":
@@ -32,16 +31,16 @@ class Zimuzu_site:
         print('Signing...')
         client_id = str(uuid.uuid1())
         self.headers = {'Accept': ' application/json, text/javascript, */*; q=0.01',
-                   'X-DevTools-Emulate-Network-Conditions-Client-Id': client_id,
-                   'X-Requested-With': 'XMLHttpRequest',
-                   'X-FirePHP-Version': '0.0.6',
-                   'Host': 'www.zimuzu.tv',
-                   'Origin': 'http://www.zimuzu.tv',
-                   'Referer': 'http://www.zimuzu.tv/user/login',
-                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                 'Chrome/42.0.2311.90 Safari/537.36',
-                   'Accept-Encoding': ' gzip, deflate, sdch',
-                   'Accept-Language': ' zh-CN,zh;q=0.8'}
+                        'X-DevTools-Emulate-Network-Conditions-Client-Id': client_id,
+                        'X-Requested-With': 'XMLHttpRequest',
+                        'X-FirePHP-Version': '0.0.6',
+                        'Host': 'www.zimuzu.tv',
+                        'Origin': 'http://www.zimuzu.tv',
+                        'Referer': 'http://www.zimuzu.tv/user/login',
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/42.0.2311.90 Safari/537.36',
+                        'Accept-Encoding': ' gzip, deflate, sdch',
+                        'Accept-Language': ' zh-CN,zh;q=0.8'}
         res_headers = requests.get('http://www.zimuzu.tv/user/login', headers=self.headers).headers
         session = res_headers['set-cookie'][10:36]
         self.headers['Cookie'] = 'PHPSESSID='+session+'; CNZZDATA1254180690=784053439-1449042469-%7C1449047872'
@@ -75,7 +74,7 @@ class Zimuzu_site:
             season = int(one['season'])
             episode = int(one['episode'])
             if (season == play['season'] and episode > play['episode']) or \
-                    (season > play['season'] and episode != 0 and season < 100):
+                    (100 > season > play['season'] and episode != 0):
                 link = one.find('a', href=re.compile('ed2k:'))
                 uri = str(link['href'])
                 if season not in seasons:
